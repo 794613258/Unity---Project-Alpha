caca6808f4489a1102e653d3f9a858dc50fb2448
794613258 <Louis.dubeux@gmail.com>
2019-02-09 20:57:40 +0100

    basic controler movement added

---
+++ Assets/02 - Scripts/01 - Camera/CameraPosition.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPosition : MonoBehaviour
+{
+
+    [SerializeField]
+    public GameObject player;
+    public float _yOffset = 10.0f;
+    public float _zOffset = 5.5f;
+    public float _xOfset = 0.0f;
+    private Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //float newXPosition = player.transform.position.x - offset.x;
+        //float newZPosition = player.transform.position.z - offset.z;
+        this.transform.position = new Vector3(
+            player.transform.position.x, 
+            player.transform.position.y + _yOffset, 
+            player.transform.position.z - _zOffset
+            );
+    }
+}
---
+++ Assets/02 - Scripts/01 - Player/Movement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField]
+    public float speed = 6.0f;
+    public float rotSpeed = 90.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+
+    private Vector3 moveDirection = Vector3.zero;
+    private CharacterController controller;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+
+        // let the gameObject fall down
+        //gameObject.transform.position = new Vector3(0, 5, 0);
+    }
+
+    void Update()
+    {
+        if (controller.isGrounded)
+        {
+            // We are grounded, so recalculate
+            // move direction directly from axes
+
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection = moveDirection * speed;
+
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+
+        // Apply gravity
+        moveDirection.y = moveDirection.y - (gravity * Time.deltaTime);
+
+        // Move the controller
+        controller.Move(moveDirection * Time.deltaTime);
+        //transform.Rotate(0, Input.GetAxis("Horizontal") * rotSpeed * Time.deltaTime, 0);
+        //moveDirection.y += 1;
+        //this.gameObject.transform.LookAt(this.transform.position + moveDirection * Time.deltaTime);
+    }
+}
